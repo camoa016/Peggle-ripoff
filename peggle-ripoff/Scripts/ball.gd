537bc14b9689f04f2cec081a1096221b11cca6b6
camoa016 <carstenbmoen2008@gmail.com>
2026-04-21 10:23:23 +0200

    Add borders scene, floor cleanup, and scoring
    Add a Borders scene and Floor.gd Area2D script to detect bodies leaving the play area and queue_free() any nodes in the "Balls" group. Tag Ball instances with group "Balls" and reduce gravity_scale to 0.75. Increment Globals.score when a ball hits a peg and preload the peg scene in globals.gd. Update main.tscn to instance the new borders scene, set camera limits, and adjust Peg/Launcher positions. Register the "Balls" group in project.godot.

--- peggle-ripoff/Scripts/ball.gd
+++ peggle-ripoff/Scripts/ball.gd
@@ -2,9 +2,12 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	var direction = (get_global_mouse_position() - global_position).normalized()
 	apply_central_impulse(direction * 500)
 
 func _on_body_entered(body):
 	if body.is_in_group("Pegs"):
 		body.on_hit()
+		Globals.score += 10
+		print(Globals.score)
--- peggle-ripoff/Scripts/globals.gd
+++ peggle-ripoff/Scripts/globals.gd
@@ -1,5 +1,5 @@
 extends Node
 
 @onready var ball_scene = preload("res://Scenes/ball.tscn")
-
+@onready var peg_scene = preload("res://Scenes/peg.tscn")
 @onready var score = 0
