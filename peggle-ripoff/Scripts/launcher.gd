0ade1504bfaaa2f2e319421ff56485dd927b4dd3
Sivert54 <austrudsivert@gmail.com>
2026-05-19 09:21:20 +0200

    Add ball limit and update Media resource paths
    Introduce a total_balls counter (default 10) and guard shoot_ball() so it only instantiates a ball when remaining ammo > 0, decrementing the counter after each shot. Also fix resource paths in launcher.tscn to use the correct 'res://Media/...' capitalization for the cannon textures to match the filesystem and avoid missing resource errors.

--- peggle-ripoff/Scripts/launcher.gd
+++ peggle-ripoff/Scripts/launcher.gd
@@ -1,15 +1,17 @@
 extends Node2D
-
+var total_balls = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func shoot_ball():
-	var ball = Globals.ball_scene.instantiate()
-	add_child(ball)
-	ball.add_to_group("Balls")
-	ball.reparent(get_tree().current_scene)
+	if(total_balls > 0):
+		var ball = Globals.ball_scene.instantiate()
+		add_child(ball)
+		ball.add_to_group("Balls")
+		ball.reparent(get_tree().current_scene)
+		total_balls -= 1
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
